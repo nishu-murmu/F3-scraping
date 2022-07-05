6d24092d318065c62142a555fd53ea32ea97c798
nishu-murmu <nishumurmu017@gmail.com>
2022-07-05 15:41:06 +0530

    feat: adding basic backend project & application

--- scrapeApp/scrapeApp/asgi.py
+++ scrapeApp/scrapeApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for scrapingBackend project.
+ASGI config for scrapeApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapingBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapeApp.settings')
 
 application = get_asgi_application()
--- scrapeApp/scrapeApp/wsgi.py
+++ scrapeApp/scrapeApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for scrapingBackend project.
+WSGI config for scrapeApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapingBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapeApp.settings')
 
 application = get_wsgi_application()
