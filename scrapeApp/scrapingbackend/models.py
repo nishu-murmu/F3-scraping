1ed1814594b66f9a5f2bc8cd7545c1b9192d4de3
nishu-murmu <nishumurmu017@gmail.com>
2022-07-06 15:04:41 +0530

    feat(public): updating favicon and adding model in django app

--- scrapeApp/scrapingbackend/models.py
+++ scrapeApp/scrapingbackend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Party(models.Model):
+    username = models.EmailField()
+    password = models.CharField()
--- scrapeApp/scrapingbackend/views.py
+++ scrapeApp/scrapingbackend/views.py
@@ -1,7 +1,32 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse  
-  
-def hello(request):  
-    return HttpResponse("<h2>Hello, Welcome to Nishu!</h2>")  
+from django.template import loader
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.http import require_http_methods
+
+
+def index(request):
+    return HttpResponse('<h1>Main website</h1>')
+
+
+def main(request):
+    template = loader.get_template('index.html')
+    name = {
+        'student': 'Nishu'
+    }
+    return HttpResponse(template.render(name))
+
+
+def party(request):
+    a = True
+    if a:
+        return HttpResponse("<h2>Hello, Welcome Nishu!</h2>")
+    else:
+        return HttpResponseNotFound('<h2>Error not found!</h2>')
+
+
+@require_http_methods(['GET'])
+def show(request):
+    return HttpResponse('<h2>This is a GET request!</h2>')
